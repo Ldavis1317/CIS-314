3d0f2129e9e508ba8a503f347e444cd55869a560
Luke Davis <ldavis1311@hotmail.com>
2024-10-23 00:10:20 -0400

    second commit

--- passwordhardener.py
+++ passwordhardener.py
@@ -1,18 +1,19 @@
 import re
 
-password_file = "password_file.txt"
+password_file = "password_file.txt" # variable storing a text file where weak passwords will be written
 
 def password_strength(password):
-    strength = 0
-    feedback = []
+    strength = 0 # intialize the password strength
+    feedback = [] # empty list to store feedback
 
-    if len(password) >=8:
-        strength +=1
+
+    if len(password) >=8: # check if password is 8 characters long
+        strength +=1 # if true increase strength by 1
     else:
-        feedback.append("Password should be at least 8 characters long.")
+        feedback.append("Password should be at least 8 characters long.")  # if not return this
     
-    if re.search(r'[a-z]', password):
-        strength+=1
+    if re.search(r'[a-z]', password): # uses regular expressions to check if password includes a lowercase letter
+        strength+=1 
     else: 
         feedback.append("Password needs at least one lowercase letter.")
     
